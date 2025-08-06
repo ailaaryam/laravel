edb9d95570ca76b9cddf60afe85a81111e0a9e2a
ailaaryam <ailaaryam141@gmail.com>
2025-08-06 16:25:37 -0300

    arrumar

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use App\Http\Requests\StoreUsuarioRequest;
+use App\Http\Requests\UpdateUsuarioRequest;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    function registrar(Request $dados){
+        return $dados;
+    }
+
+    function login(Request $dados){}
+
+    function logout(Request $dados){}
+
+    function desativarConta(Request $dados){}
+
+    function fotoUpload(Request $dados){}
+
+    function editar(Request $dados){}
+
+    function perfil(Request $dados){}
+
+}
